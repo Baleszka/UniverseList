465786fb5edc65552f62b1fea3d5917954498fd8
Baleszka <baleszkadev@gmail.com>
2025-09-10 22:35:21 +0200

    Added files

---
+++ js/routes.js
@@ -0,0 +1,11 @@
+import List from './pages/List.js';
+import Leaderboard from './pages/Leaderboard.js';
+import Roulette from './pages/Roulette.js';
+import ill from './pages/ill.js'
+
+export default [
+    { path: '/', component: List },
+    { path: '/leaderboard', component: Leaderboard },
+    { path: '/roulette', component: Roulette },
+    { path: '/ill', component: ill },
+];
