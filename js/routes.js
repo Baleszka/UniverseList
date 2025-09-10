e81d5e2a10925336c3e6f1dcb9ddad5dfb53e53d
Baleszka <baleszkadev@gmail.com>
2025-09-10 22:53:28 +0200

    added ILL leaderboard

--- js/routes.js
+++ js/routes.js
@@ -2,10 +2,12 @@ import List from './pages/List.js';
 import Leaderboard from './pages/Leaderboard.js';
 import Roulette from './pages/Roulette.js';
 import ill from './pages/ill.js'
+import IllLeaderboard from './pages/illleaderboard.js';
 
 export default [
     { path: '/', component: List },
     { path: '/leaderboard', component: Leaderboard },
     { path: '/roulette', component: Roulette },
     { path: '/ill', component: ill },
+    { path: '/illleaderboard', component: IllLeaderboard },
 ];
